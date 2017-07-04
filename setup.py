139b5b0f21e3bf102cd3d4b28bdf42896a181595
LyricLy <hanson.twentytwo@gmail.com>
2017-07-04 12:32:01 +1200

    stuff

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'hastebin.py',
+  packages = ['hastebin'],
+  version = '0.2',
+  description = 'A simple Hastebin API wrapper for Python.',
+  author = 'LyricLy',
+  author_email = 'hanson.twentytwo@gmail.com',
+  url = 'https://github.com/LyricLy/hastebin.py',
+  download_url = 'https://github.com/LyricLy/hastebin.py/archive/0.1.tar.gz',
+  keywords = ['hastebin'],
+  install_requires = ['requests']
+)
