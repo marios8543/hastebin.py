07b43e80b603eb8dc3e1ec610b526042ab197af6
Nihaal Sangha <nihaal.s@live.co.uk>
2017-12-02 17:15:11 +0000

    Encodes as unicode

--- hastebin/hastebin.py
+++ hastebin/hastebin.py
@@ -1,5 +1,5 @@
 import requests
 
 def post(content):
-    post = requests.post("https://hastebin.com/documents", data=content)
+    post = requests.post("https://hastebin.com/documents", data=content.encode('utf-8'))
     return "https://hastebin.com/" + post.json()["key"]
